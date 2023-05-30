21f0824ead28ad1c1baed692b2fe3246cc14784a
darranda <darrandaf@gmail.com>
2023-05-30 16:00:26 -0400

    algorithms

---
+++ src/main/java/com/writingalgorithms/QuickSort.java
@@ -0,0 +1,44 @@
+package com.writingalgorithms;
+
+public class QuickSort {
+    public static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+
+            if (arr[j] <= pivot) {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, i + 1, high);
+        return i + 1;
+    }
+
+    public static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pivotIndex = partition(arr, low, high);
+            quickSort(arr, low, pivotIndex - 1);
+            quickSort(arr, pivotIndex + 1, high);
+        }
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {4, 1, 3, 9, 7};
+
+        quickSort(arr, 0, arr.length - 1);
+        System.out.println();
+        System.out.println("This is the sorted Array");
+        for (int num : arr) {
+            System.out.println(num + " ");
+        }
+        System.out.println();
+    }
+}
